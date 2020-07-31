d75020b27b6b3413abc20b3ca214d3378096a83e
julianortega <julianortega@MacBook-de-julianortega.local>
2020-07-31 16:43:05 +0200

    models

---
+++ models/item.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+  name: String,
+  userId: {type: mongoose.Types.ObjectId, ref:"User"},
+  description: String,
+  requests: [{
+
+      requester: {type: mongoose.Types.ObjectId, ref:"User"}, 
+      delivery: {type: String, enum:["pickup", "send"]},
+
+  }],
+  image: { type: String ,default: ""},
+  category: {type: String, enum:["books", "clothes", "devices"]},
+  city: {type: String, enum:["Barcelona", "Madrid"]},
+});
+
+
+
+const Item = mongoose.model('item', itemSchema);
+
+module.exports = Item;
--- models/user.js
+++ models/user.js
@@ -5,8 +5,11 @@ const userSchema = new Schema({
   name: String,
   email: String,
   password: String,
-  isLaunderer: { type: Boolean, default: false },
-  fee: { type: Number, default: null }
+  description: String,
+  image: { type: String ,default: ""},
+  myitems: [{type: mongoose.Types.ObjectId, ref:"Item"}],
+  acquireditems: [{type: mongoose.Types.ObjectId, ref:"Item"}],
+  favourites:[{type: mongoose.Types.ObjectId, ref:"Item"}],
 });
 
 userSchema.set('timestamps', true);
